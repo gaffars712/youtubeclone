e9e9c644488b8199877d3845a0ac0efe70ca1a6b
ShaikhGaffar <134927774+gaffars712@users.noreply.github.com>
2025-02-04 10:41:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import './App.css';
 import { Box } from '@mui/material';
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './compunent/Navbar';
 import Feed from './compunent/Feed';
 import Videodetails from './compunent/Videodetails';
 import Channeldetails from './compunent/Channeldetails';
 import Serchfeed from './compunent/Serchfeed';
+import { HelmetProvider } from 'react-helmet-async';
 
 
 function App() {
-  return(
-  <BrowserRouter>
-  <Box sx={{background:"#000"}}>
-    <Navbar/>
+  return (
+    <HelmetProvider>
+      <BrowserRouter>
+        <Box sx={{ background: "#000" }}>
+          <Navbar />
 
-    <Routes>  
-      <Route exact path='/' element={<Feed/>}/>
-      <Route exact path='/video/:id' element={<Videodetails/>}/>
-      <Route exact path='/channel/:id' element={<Channeldetails/>}/>
-      <Route exact path='/search/:searchtem' element={<Serchfeed/>}/>
-
-
-
+          <Routes>
+            <Route exact path='/' element={<Feed />} />
+            <Route exact path='/video/:id' element={<Videodetails />} />
+            <Route exact path='/channel/:id' element={<Channeldetails />} />
+            <Route exact path='/search/:searchtem' element={<Serchfeed />} />
 
 
-    </Routes>
-  </Box> 
-  </BrowserRouter>
+
+
+
+          </Routes>
+        </Box>
+      </BrowserRouter>
+    </HelmetProvider>
   )
 }
 
