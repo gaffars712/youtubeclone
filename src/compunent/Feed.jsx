8eb992a7a4dfb15e0ef93c344413aeb9d685f5f2
ShaikhGaffar <134927774+gaffars712@users.noreply.github.com>
2025-02-04 10:33:32 +0530

    update

--- src/compunent/Feed.jsx
+++ src/compunent/Feed.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { useState,useEffect } from 'react';
-import { Box,Stack,Typography } from '@mui/material';
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 import Sidebaar from './Sidebaar'
 import Videos from './Videos';
 import { fetchFromAPI } from './FachfromAPI';
+import { Helmet } from 'react-helmet-async';
 
 const Feed = () => {
   const [selectedcetagry, setselectedcetagry] = useState('New')
@@ -13,26 +14,30 @@ const Feed = () => {
     fetchFromAPI(`search?part=snippet&q=${selectedcetagry}`)
       .then((data) => setvideos(data.items))
 
-    }, [selectedcetagry]);
+  }, [selectedcetagry]);
 
   const Fullyear = new Date().getFullYear()
-  return (
-    <Stack sx={{ flexDirection:{sx:"column",md:"row"}}}>
-      <Box sx={{height:{sx:'auto',md:'92vh'}, BorderRight:"1px solid #3d3d3d",px:{sx:0,md:2}}}>
+  return (<>
+    <Helmet>
+      <title>Home - YouTubeClone</title>
+      <meta name="keyword" content="youtube,gaffar portfolio, youtubeClone,YouTubeClone, youtube, clone, clone,gaffar," />
+    </Helmet>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh' }, BorderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
         <Sidebaar selectedcetagry={selectedcetagry} setselectedcetagry={setselectedcetagry} />
-        <Typography className='copyright' variant='body2' sx={{color:"#fff", }}>
+        <Typography className='copyright' variant='body2' sx={{ color: "#fff", }}>
           Copyright {Fullyear} Gaffar Clone.
 
         </Typography>
       </Box>
-      <Box  p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }} >
-      <Typography variant='h4' fontWeight={'bold'} sx={{color:"white",mb:2}}> 
-      {selectedcetagry} <span style={{color:"#f31503"}}>Video</span>
-      </Typography>
-      <Videos vides={videos}/>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }} >
+        <Typography variant='h4' fontWeight={'bold'} sx={{ color: "white", mb: 2 }}>
+          {selectedcetagry} <span style={{ color: "#f31503" }}>Video</span>
+        </Typography>
+        <Videos vides={videos} />
       </Box>
-        </Stack>
-  )
+    </Stack>
+  </>)
 }
 
 export default Feed;
--- src/compunent/Videodetails.jsx
+++ src/compunent/Videodetails.jsx
@@ -2,63 +2,69 @@ import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import ReactPlayer from 'react-player';
-import { Typography , Stack , Box } from '@mui/material';
+import { Typography, Stack, Box } from '@mui/material';
 import { CheckCircle, FactCheck } from '@mui/icons-material';
 import Videos from './Videos';
 import { fetchFromAPI } from './FachfromAPI';
+import { Helmet } from 'react-helmet-async';
 const Videodetails = () => {
   const [videosdetails, setvideosdetails] = useState(null);
   const [videos, setvideos] = useState(null)
-  const {id} = useParams();
+  const { id } = useParams();
 
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statics&id=${id}`)
-    .then((data)=> setvideosdetails(data.items[0]) )
+      .then((data) => setvideosdetails(data.items[0]))
 
     fetchFromAPI(`search?part=snippet&relatedToVideoid=${id}&type=video`)
-    .then((data)=>setvideos(data.items))
-  },[id])
+      .then((data) => setvideos(data.items))
+  }, [id])
 
-  if(!videosdetails?.snippet)return ' loding...'
-    const {snippet:{title, channelId , channelTitle},statistics:{viewCount,likeCount}}=videosdetails;
-  return (
+  if (!videosdetails?.snippet) return ' loding...'
+  const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount } } = videosdetails;
+  return (<>
+    <Helmet>
+      <title>Video - YouTubeClone</title>
+      <meta name="keyword" content="youtube,gaffar portfolio, youtubeClone,YouTubeClone, youtube, clone, clone,gaffar," />
+
+    </Helmet>
     <Box minHeight='96vh'>
-      <Stack direction={{xs:'column', md:'row'}}>
+      <Stack direction={{ xs: 'column', md: 'row' }}>
         <Box flex={1}>
-          <Box sx={{width:"100%", top:"95px"}}>
-            <ReactPlayer  className='react-player' url={`https://www.youtube.com/watch?v=${id}`} controls/>
+          <Box sx={{ width: "100%", top: "95px" }}>
+            <ReactPlayer className='react-player' url={`https://www.youtube.com/watch?v=${id}`} controls />
             <Typography color={'#fff'} fontWeight={'bold'} variant='h5' p={'2'}>
-            {title}
-              </Typography>
-              <Stack direction={'row'} justifyContent={'space-between'} sx={{color:"#fff"}} py={1} px={2}>
+              {title}
+            </Typography>
+            <Stack direction={'row'} justifyContent={'space-between'} sx={{ color: "#fff" }} py={1} px={2}>
               <Link to={`/channel/${channelId}`}>
-                <Typography variant={{sm:'subtitle',md:'h6'}} color={'#fff'}>
+                <Typography variant={{ sm: 'subtitle', md: 'h6' }} color={'#fff'}>
                   {channelTitle}
-                  <CheckCircle sx={{ fontSize:"12px",color:"gray" } } />
+                  <CheckCircle sx={{ fontSize: "12px", color: "gray" }} />
                 </Typography>
               </Link>
               <Stack direction={'row'} gap={'20px'} alignItems={'center'}>
-                <Typography variant='body1' sx={{opacity:"0.7"}}>
-                  {parseInt(viewCount).toLocaleString()}  Views 
+                <Typography variant='body1' sx={{ opacity: "0.7" }}>
+                  {parseInt(viewCount).toLocaleString()}  Views
                 </Typography>
-                <Typography variant='body1' sx={{opacity:"0.7"}}>
-                  {parseInt(likeCount).toLocaleString()}  Likes 
+                <Typography variant='body1' sx={{ opacity: "0.7" }}>
+                  {parseInt(likeCount).toLocaleString()}  Likes
                 </Typography>
               </Stack>
-              </Stack>
-              
+            </Stack>
+
           </Box>
 
         </Box>
-      <Box px={2} py={{md:1,xs:5}} justifyContent={'center' } alignItems={'center'}>
-         <Videos vides={videos} direction='column'/>
+        <Box px={2} py={{ md: 1, xs: 5 }} justifyContent={'center'} alignItems={'center'}>
+          <Videos vides={videos} direction='column' />
 
-      </Box>
+        </Box>
       </Stack>
-      
+
     </Box>
-  )
+  </>)
 }
 
 export default Videodetails;
